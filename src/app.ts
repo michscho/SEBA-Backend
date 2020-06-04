897f91f2e99fb344836836fd2119315ecbc395b7
Nikan <52320631+nikanmog@users.noreply.github.com>
2020-06-04 15:18:10 +0200

    Added Swagger Framework

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,35 @@ const middleware = require('./middleware');
 const bodyParser = require('body-parser');
 const helmet     = require('helmet');
 const config     = require('./config');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 const authentication  = require('./routes/authentication');
 const course = require('./routes/course');
 const app = express();
 
+
+
+var swaggerDefinition = {
+    info: {
+        title: 'Node Swagger API',
+        version: '1.0.0'
+    },
+    host: 'localhost:3000',
+    basePath: '/',
+};
+
+// options for swagger jsdoc
+var options = {
+    swaggerDefinition: swaggerDefinition, // swagger definition
+    apis: ['app.ts', 'routes/*.js'], // path where API specification are written
+};
+
+// initialize swaggerJSDoc
+var swaggerSpec = swaggerJsDoc(options);
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 // Middleware
 app.use(helmet());
 app.use(bodyParser.json());
--- src/models/sequelize.ts
+++ src/models/sequelize.ts
@@ -1,11 +1,12 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
-  const sequelize = sequelize.define('sequelize', {
+  const seq = sequelize.define('sequelize', {
     name: DataTypes.STRING,
     email: DataTypes.STRING
   }, {});
-  sequelize.associate = function(models) {
+  seq.associate = function(models) {
     // associations can be defined here
   };
-  return sequelize;
+  return seq;
 };
