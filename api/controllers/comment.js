554de2bcfb8e1ecdf42893282081d1fe7c627732
ga87fax <schworm@in.tum.de>
2020-06-16 20:29:00 +0200

    add controller for comment, change router for comment

---
+++ api/controllers/comment.js
@@ -0,0 +1,144 @@
+const mongoose = require("mongoose");
+const Comment = require("../models/comment");
+
+exports.comments_get_all = (req, res, next) => {
+    Comment.find()
+        .select("_id name  creationDate content creator belonging")
+        .exec()
+        .then(docs => {
+            const response = {
+                count: docs.length,
+                items: docs.map(doc => {
+                    return {
+                        _id: doc._id,
+                        name: doc.name,
+                        creationDate: doc.creationDate,
+                        content: doc.content,
+                        creator: doc.creator,
+                        belonging: doc.belonging,
+                        request: {
+                            type: "GET",
+                            url: "http://localhost:9000/comments/" + doc._id
+                        }
+                    };
+                })
+            };
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+};
+
+exports.create_comment = (req, res, next) => {
+    const comment = new Comment({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        creationDate: req.body.creationDate,
+        content: req.body.content,
+        creator: req.body.creator,
+        belonging: req.body.belonging,
+    });
+    comment
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: "Created comment successfully",
+                createdContentItem: {
+                    _id: new result.id,
+                    name: result.name,
+                    creationDate: result.creationDate,
+                    content: result.content,
+                    creator: result.creator,
+                    belonging: result.belonging,
+                    request: {
+                        type: "GET",
+                        url: "http://localhost:9000/comments/" + result._id
+                    }
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+};
+
+exports.comments_get_comment = (req, res, next) => {
+    const id = req.params.commentId;
+    Comment.findById(id)
+        .select("_id name  creationDate content creator belonging")
+        .exec()
+        .then(doc => {
+            console.log("From database", doc);
+            if (doc) {
+                res.status(200).json({
+                    comment: doc,
+                    request: {
+                        type: "GET",
+                        url: "http://localhost:9000/comments"
+                    }
+                });
+            } else {
+                res
+                    .status(404)
+                    .json({message: "No valid entry found for provided ID"});
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err});
+        });
+};
+
+exports.comments_update_comment = (req, res, next) => {
+    const id = req.params.commentId;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Comment.update({_id: id}, {$set: updateOps})
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: "Comment updated",
+                request: {
+                    type: "GET",
+                    url: "http://localhost:9000/comments/" + id
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+};
+
+exports.comments_delete = (req, res, next) => {
+    const id = req.params.commentId;
+    Comment.remove({_id: id})
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: "Comment deleted",
+                request: {
+                    type: "POST",
+                    url: "http://localhost:9000/comments",
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+};
--- api/routes/comment.js
+++ api/routes/comment.js
@@ -1,16 +1,43 @@
 const express = require("express");
 const router = express.Router();
+const multer = require('multer');
 const checkAuth = require('../middleware/check-auth');
+const CommentController = require('../controllers/comment');
 
-const CommentsController = require('../controllers/comments');
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function(req, file, cb) {
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
 
-// Handle incoming GET requests to /orders
-// router.get("/", checkAuth, OrdersController.orders_get_all);
-//
-// router.post("/", checkAuth, OrdersController.orders_create_order);
-//
-// router.get("/:orderId", checkAuth, OrdersController.orders_get_order);
-//
-// router.delete("/:orderId", checkAuth, OrdersController.orders_delete_order);
+const fileFilter = (req, file, cb) => {
+    // reject a file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
+
+router.get("/", CommentController.comments_get_all);
+
+router.post("/", CommentController.create_comment);
+
+router.get("/:commentId", CommentController.comments_get_comment);
+
+router.patch("/:commentId", CommentController.comments_update_comment);
+
+router.delete("/:commentId", CommentController.comments_delete);
 
 module.exports = router;
