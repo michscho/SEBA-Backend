bf9ec434fc1b369ad205ba4a5671b8f5ed9bef37
ga87fax <schworm@in.tum.de>
2020-07-09 12:05:44 +0200

    update comment model

--- api/models/comment.js
+++ api/models/comment.js
@@ -2,11 +2,10 @@ const mongoose = require('mongoose');
 
 const commentSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: {type: String, required: true},
-    creationDate: {type: Date, required: true},
-    content: {type: String, required: true},
+    title: {type: String, required: true},
+    text: {type: String, required: true},
     creator: {type: String, required: true},
-    belonging: {type: String, required: true},
+    contentId: {type: String, required: true},
 });
 
 module.exports = mongoose.model('Comment', commentSchema);
