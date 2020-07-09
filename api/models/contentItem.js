8d13b357dd07f736cfe6f199b9c6a0cdcc9ab697
ga87fax <schworm@in.tum.de>
2020-07-09 11:02:20 +0200

    update model contentItem

--- api/models/contentItem.js
+++ api/models/contentItem.js
@@ -2,11 +2,9 @@ const mongoose = require('mongoose');
 
 const courseItemSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: { type: String, required: true },
+    title: { type: String, required: true },
     source: { type: String, required: true },
-    author: { type: String, required: true },
-    isFree: {type: Boolean, required: true},
-    price: {type: Number, required: false},
+    description: {type: String, required: true},
 });
 
 module.exports = mongoose.model('ContentItem', courseItemSchema);
