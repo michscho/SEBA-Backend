78bfdea4c8681a7814ea549c78b1a4dff314eb3f
Nikan <52320631+nikanmog@users.noreply.github.com>
2020-07-16 22:39:19 +0200

    Minor bugfix

--- api/controllers/course.js
+++ api/controllers/course.js
@@ -74,7 +74,7 @@ exports.create_course = (req, res, next) => {
         difficulty: req.body.difficulty,
         description: req.body.description,
         contentItems: items,
-        rating: 0,
+        rating: [],
         price: req.body.price,
     });
     course
